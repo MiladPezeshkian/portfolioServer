616b2beb4cd5b62616d1a90296fa124381b87a33
Milad Pez <miladpez10@gmail.com>
2025-08-04 03:24:09 +0330

    آپدیت

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ app.set("trust proxy", 1);
 // Implement CORS
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://drparastofatthi.netlify.app"],
+    origin: ["http://localhost:5173", "https://lonewalkerProf.netlify.app"],
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS", "PUT"],
     credentials: true,
     allowedHeaders: [
